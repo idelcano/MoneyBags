4bf1f0208677e057a60a5953a19f228cd574d8b0
Idelcano <ignaciodelcano@gmail.com>
2019-01-01 23:56:26 +0100

    added main activity presenter and money bag list logic

--- app/src/main/java/com/idelcano/moneycontrol/moneycontrol/MainActivity.kt
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/MainActivity.kt
@@ -3,43 +3,74 @@ package com.idelcano.moneycontrol.moneycontrol
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
+import com.idelcano.moneycontrol.moneycontrol.data.repositories.MoneyBagRepository
+import com.idelcano.moneycontrol.moneycontrol.domain.entity.MoneyBag
+import com.idelcano.moneycontrol.moneycontrol.domain.usecase.GetMoneyBagsUseCase
 import com.idelcano.moneycontrol.moneycontrol.fragments.MoneyBagDialogFragment
+import com.idelcano.moneycontrol.moneycontrol.presentation.executers.CoroutinesExecutor
+import com.idelcano.moneycontrol.moneycontrol.presentation.presenters.MainActivityPresenter
+import com.idelcano.moneycontrol.moneycontrol.presentation.presenters.adapters.MoneyBagAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainActivityPresenter.View {
 
+    var presenter : MainActivityPresenter = MainActivityPresenter()
+    lateinit var adapter : MoneyBagAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        initializeFabAction()
+        initializeRecyclerView()
+        initializePresenter()
+    }
 
+    private fun initializeFabAction() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, R.string.create_money, Snackbar.LENGTH_LONG)
-                    .setAction(R.string.add, View.OnClickListener {
-                        val ft = this@MainActivity.supportFragmentManager
-                        val dialogFragment = MoneyBagDialogFragment()
-                        dialogFragment.show(ft, dialogFragment.TAG_DIALOG)
-                    }).show()
+                .setAction(R.string.add, View.OnClickListener {
+                    val ft = this@MainActivity.supportFragmentManager
+                    val dialogFragment = MoneyBagDialogFragment()
+                    dialogFragment.show(ft, dialogFragment.TAG_DIALOG)
+                }).show()
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    override fun showMoneyBags(moneyBags: List<MoneyBag?>) {
+        adapter.setMoneyBags(moneyBags)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    override fun clearMoneyBags() {
+        adapter.clearMoneyBags()
+    }
+
+    override fun showLoading() {
+        progress_bar.visibility = View.VISIBLE
+        header_text.text =getString(R.string.loading_list_text);
+    }
+
+    override fun hideLoading() {
+        progress_bar.visibility = View.GONE
+    }
+
+    override fun showTotalMoneyBags(count: Int) {
+        header_text.text =  String.format(getString(R.string.header_text),count)
+    }
+
+    private fun initializeRecyclerView() {
+        this.adapter = MoneyBagAdapter() { item ->
+            presenter.onMoneyBagClicked(item)
         }
+        recycler.adapter = adapter
+    }
+
+    private fun initializePresenter() {
+        presenter.initPresenter(this, GetMoneyBagsUseCase(MoneyBagRepository(), CoroutinesExecutor()))
+    }
+
+    override fun onDestroy() {
+        presenter.detachView()
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/presentation/presenters/MainActivityPresenter.kt
@@ -0,0 +1,51 @@
+package com.idelcano.moneycontrol.moneycontrol.presentation.presenters
+
+import com.idelcano.moneycontrol.moneycontrol.MainActivity
+import com.idelcano.moneycontrol.moneycontrol.domain.entity.MoneyBag
+import com.idelcano.moneycontrol.moneycontrol.domain.usecase.GetMoneyBagsUseCase
+
+
+class MainActivityPresenter{
+    lateinit var getMoneyBagListUseCase : GetMoneyBagsUseCase
+    var view : MainActivity? = null
+
+    fun initPresenter(view : MainActivity, getMoneyBagListUseCase : GetMoneyBagsUseCase) {
+        this.view = view
+        this.getMoneyBagListUseCase = getMoneyBagListUseCase
+        loadingMoneyBags()
+        loadMoneyBags()
+    }
+
+    private fun loadMoneyBags() {
+        getMoneyBagListUseCase.execute {
+            showMoneyBags(it)
+        }
+    }
+
+    private fun loadingMoneyBags() {
+        view?.showLoading()
+        view?.clearMoneyBags()
+    }
+
+    private fun showMoneyBags(moneyBags: List<MoneyBag?>) {
+        view?.hideLoading()
+        view?.showMoneyBags(moneyBags)
+        view?.showTotalMoneyBags(moneyBags.size)
+    }
+
+    fun detachView() {
+        view = null
+    }
+
+    fun onMoneyBagClicked(item: MoneyBag) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    interface View {
+        fun showMoneyBags(moneyBags: List<MoneyBag?>)
+        fun clearMoneyBags()
+        fun showLoading()
+        fun hideLoading()
+        fun showTotalMoneyBags(count: Int)
+    }
+}
