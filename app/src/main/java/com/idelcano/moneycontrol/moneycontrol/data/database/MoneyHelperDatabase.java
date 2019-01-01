c4aceb982c40710e53edafb1b86a90545d75ce0b
Idelcano <ignaciodelcano@gmail.com>
2019-01-01 23:54:35 +0100

    remove deprecated annotation

--- app/src/main/java/com/idelcano/moneycontrol/moneycontrol/data/database/MoneyHelperDatabase.java
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/data/database/MoneyHelperDatabase.java
@@ -2,8 +2,9 @@ package com.idelcano.moneycontrol.moneycontrol.data.database;
 
 import com.raizlabs.android.dbflow.annotation.Database;
 
-@Database(name = MoneyHelperDatabase.NAME, version = MoneyHelperDatabase.VERSION)
+@Database(version = MoneyHelperDatabase.VERSION)
 public class MoneyHelperDatabase {
+    private MoneyHelperDatabase(){}
     public static final String NAME = "MoneyHelperDB";
 
     public static final int VERSION = 1;
