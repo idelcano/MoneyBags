ee128b8b77de2863099a02e8caf6b6bb255a44c5
Idelcano <ignaciodelcano@gmail.com>
2019-01-01 23:55:57 +0100

    added use case to get money bag list

---
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/domain/usecase/GetMoneyBagsUseCase.kt
@@ -0,0 +1,20 @@
+package com.idelcano.moneycontrol.moneycontrol.domain.usecase
+
+import com.idelcano.moneycontrol.moneycontrol.data.repositories.MoneyBagRepository
+import com.idelcano.moneycontrol.moneycontrol.domain.boundary.Executor
+import com.idelcano.moneycontrol.moneycontrol.domain.entity.MoneyBag
+
+
+class GetMoneyBagsUseCase (private val moneyBagRepository: MoneyBagRepository,
+                           private val executor: Executor): UseCase(executor) {
+
+        fun execute(onResult: (List<MoneyBag?>) -> Unit) {
+            asyncExecute {
+                val moneyBags = moneyBagRepository.getAll()
+
+                uiExecute {
+                    onResult(moneyBags)
+                }
+            }
+        }
+    }
