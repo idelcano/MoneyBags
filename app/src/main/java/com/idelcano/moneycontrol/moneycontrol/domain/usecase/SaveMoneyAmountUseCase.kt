2cb847b918b54201aa34b14a650ca8faabbeb931
Idelcano <ignaciodelcano@gmail.com>
2019-01-03 00:14:16 +0100

    Added save money amount use case

---
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/domain/usecase/SaveMoneyAmountUseCase.kt
@@ -0,0 +1,18 @@
+package com.idelcano.moneycontrol.moneycontrol.domain.usecase
+
+import com.idelcano.moneycontrol.moneycontrol.data.repositories.MoneyAmountRepository
+import com.idelcano.moneycontrol.moneycontrol.domain.boundary.Executor
+import com.idelcano.moneycontrol.moneycontrol.domain.entity.MoneyAmount
+
+
+class SaveMoneyAmountUseCase (private val moneyAmountRepository: MoneyAmountRepository,
+                              private val executor: Executor): UseCase(executor) {
+
+        fun execute(moneyAmount: MoneyAmount) {
+            asyncExecute {
+                moneyAmountRepository.save(moneyAmount)
+
+                uiExecute { moneyAmount}
+            }
+        }
+    }
