0a2d8e131fab4f58f165c4a9cefd3528ec164d46
Idelcano <ignaciodelcano@gmail.com>
2019-01-02 13:33:50 +0100

    enable foreign constreints

--- app/src/main/java/com/idelcano/moneycontrol/moneycontrol/data/database/MoneyHelperDatabase.java
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/data/database/MoneyHelperDatabase.java
@@ -2,7 +2,7 @@ package com.idelcano.moneycontrol.moneycontrol.data.database;
 
 import com.raizlabs.android.dbflow.annotation.Database;
 
-@Database(version = MoneyHelperDatabase.VERSION)
+@Database(version = MoneyHelperDatabase.VERSION, foreignKeyConstraintsEnforced = true)
 public class MoneyHelperDatabase {
     private MoneyHelperDatabase(){}
     public static final String NAME = "MoneyHelperDB";
