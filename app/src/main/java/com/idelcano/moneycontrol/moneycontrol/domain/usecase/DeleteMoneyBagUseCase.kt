ff8857f1ca62907e0f2403e93649410b99d8cf0c
Idelcano <ignaciodelcano@gmail.com>
2019-01-03 00:13:47 +0100

    added delete money bag use case

---
+++ app/src/main/java/com/idelcano/moneycontrol/moneycontrol/domain/usecase/DeleteMoneyBagUseCase.kt
@@ -0,0 +1,18 @@
+package com.idelcano.moneycontrol.moneycontrol.domain.usecase
+
+import com.idelcano.moneycontrol.moneycontrol.data.repositories.MoneyBagRepository
+import com.idelcano.moneycontrol.moneycontrol.domain.boundary.Executor
+import com.idelcano.moneycontrol.moneycontrol.domain.entity.MoneyBag
+
+
+class DeleteMoneyBagUseCase (private val moneyBagRepository: MoneyBagRepository,
+                             private val executor: Executor): UseCase(executor) {
+
+        fun execute(moneyBag : MoneyBag) {
+            asyncExecute {
+                moneyBagRepository.delete(moneyBag)
+
+                uiExecute { moneyBag}
+            }
+        }
+    }
